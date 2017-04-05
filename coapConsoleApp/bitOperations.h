d0f9ab7f54702ba2aea6b761cd566e9e782523df
Jarek <jarekbutek@gmail.com>
2017-04-05 02:29:40 +0200

    delete not used files

--- coapConsoleApp/bitOperations.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Funkcja zwracaj¹ca reprezentacjê bitu z tablicy bajtow (char - 1 bajt)
-char getBit(char byte, int position);
-
-// Funkcja ustawiaj¹ca bity z byte w ustalonym miejscu. Pozycjê ustala najm³odszy bit.
-void setBits(char *src, char byte, int position);
