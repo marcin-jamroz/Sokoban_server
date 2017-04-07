2c627e55995b53ad69764d3143e3c26db2d59632
Marcin <marcin919@op.pl>
2017-04-07 03:37:31 +0200

    coap-server -> coap-resource

--- main-coap/main-coap.ino
+++ main-coap/main-coap.ino
@@ -1,27 +1,99 @@
-#include <CoAP.h>
-#include <BitOperations.h>
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 
-void setup(){
+#define MAX_BUFFER 60
 
+
+
+byte mac[] = {0x00, 0xaa, 0xbb, 0xcc, 0xde, 0xf80};
+
+EthernetUDP Udp;
+short localPort = 1242;
+
+char packetBuffer[MAX_BUFFER];
+
+RF24 radio(7, 8);
+RF24Network network(radio);
+
+uint8_t OUR_CHANNEL = 120;
+uint8_t THIS_NODE = 00;
+
+
+
+
+enum option {LampOff, LampOn, LampStatus, PotStatus};
+
+
+struct Request {
+  short option;             //0 - lampka off, 1 - lampka ON, 2 - stan lampki, 3 - stan potencjometru
+};
+
+
+struct Response {
+  short option;             //0 - lampka off, 1 - lampka ON, 2 - stan lampki, 3 - stan potencjometru
+  short value;
+};
+
+void setup() {
   Serial.begin(115200);
-  
-  char header[4] = { 0 };
-  char version = 0b01;
-  char msgType = CoAP::MessageType::ACK;
-  char len = 3;
-  char code = CoAP::ResponseCode::SERVER_ERROR;
-  int details = 1;
-  CoAP::setHeader(header, version, msgType, len, code, details, 15);
-  for (int i = 0; i < 4; i++) {
-   Serial.print("header: ");
-   Serial.println(i);
-   Serial.println(header[i], BIN);
-   
+  SPI.begin();
+  radio.begin();
+  network.begin(OUR_CHANNEL, THIS_NODE);
+  Ethernet.begin(mac);
+  Serial.println(Ethernet.localIP());
+  Udp.begin(localPort);
+
+}
+
+void loop() {
+
+int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("Jestem w if, packetSize=");
+    Serial.println(packetSize);
+
+    Udp.read(packetBuffer, MAX_BUFFER);
+
+    sendRequest(packetBuffer[0]-'0');
+  }
+ 
+  network.update();
+
+  // Nasłuchuje na wiadomości od CoAP-resource
+  while (network.available()) {
+    Serial.println("Odebrano");
+    struct Response message;
+    RF24NetworkHeader header;
+    network.read(header, &message, sizeof(struct Response));
+    Serial.println(message.option, DEC);
+    handleRequest(message.option, message.value);
   }
 }
 
+void handleRequest(short option, short value) {
 
-void loop(){
-  
+  if (option == LampStatus) {
+    Serial.println(value);
+  }
+
+  if (option == PotStatus) {
+    Serial.println(value);
+  }
 }
+
+void sendRequest(short option) {
+  RF24NetworkHeader header(01);
+  struct Request request;
+
+  request.option = option;
+
+  network.write(header, &request, sizeof(request));
+}
+
+
+
 
---
+++ main-resource/main-resource.ino
@@ -0,0 +1,85 @@
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+
+RF24 radio(7, 8);
+RF24Network network(radio);
+
+uint8_t OUR_CHANNEL = 120;
+uint8_t THIS_NODE = 01;
+
+short isLampOn = 0;
+
+
+enum option {LampOff, LampOn, LampStatus, PotStatus};
+
+
+struct Request {
+  short option;             //0 - lampka off, 1 - lampka ON, 2 - stan lampki, 3 - stan potencjometru
+};
+
+
+struct Response {
+  short option;             //0 - lampka off, 1 - lampka ON, 2 - stan lampki, 3 - stan potencjometru
+  short value;
+};
+
+void setup() {
+  Serial.begin(57600);
+  SPI.begin();
+  radio.begin();
+  network.begin(OUR_CHANNEL, THIS_NODE);
+  pinMode(3, OUTPUT);
+}
+
+void loop() {
+
+  network.update();
+
+  // Nasłuchuje na wiadomości od CoAP-server
+  while (network.available()) {
+    struct Request message;
+    RF24NetworkHeader header;
+    network.read(header, &message, sizeof(struct Request));
+    Serial.println(message.option, DEC);
+    handleRequest(message.option);
+  }
+}
+
+void handleRequest(short option) {
+  if (option == LampOff) {
+    analogWrite(3, 255);
+    isLampOn = 0;
+  }
+
+  if (option == LampOn) {
+    analogWrite(3, 0);
+    isLampOn = 1;
+  }
+
+  if (option == LampStatus) {
+    sendResponse(LampStatus, isLampOn);
+  }
+
+  if (option == PotStatus) {
+    short potentiometerValue = analogRead(0);
+    Serial.println(potentiometerValue,DEC);
+    sendResponse(PotStatus,potentiometerValue);
+  }
+}
+
+void sendResponse(short option, short value) {
+  RF24NetworkHeader header(00);
+  struct Response response;
+
+  response.option = option;
+  response.value = value;
+
+  network.write(header, &response, sizeof(response));
+  Serial.println("wysłano");
+}
+
+
+
+
