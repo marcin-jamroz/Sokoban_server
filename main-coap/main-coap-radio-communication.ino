751451271b2fc2cc51b05af72fe4672ec761d381
Patryk <glowienko.patryk@gmail.com>
2017-05-20 17:30:51 +0200

    Podział kodu na pliki, refactoring, posprzątanie kodu

---
+++ main-coap/main-coap-radio-communication.ino
@@ -0,0 +1,50 @@
+
+//=======RADIO COMMUNICATION WITH REASOURCE =======
+
+
+//wysyła request do Resourca wywołując w nim akcje zależną od opcji
+void sendRequestViaRadio(short option) { // opcja : 0 - lampka off, 1 - lampka ON, 2 - stan lampki, 3 - stan potencjometru
+  struct Request request;
+  RF24NetworkHeader header(01);
+  request.option = option;
+
+  network.write(header, &request, sizeof(request));
+}
+
+
+
+//obsługa przyjścia waidomości od resourca drogą radiową, odesłanie COAP-owej wiadomości jako response
+void handleRadioRequest(short option, short value, CoapMessage &coapMessage) {
+
+  Serial.println(value);
+  CoapMessage responseMessage;
+  uint8_t messageID[] = {201, 201};
+  unsigned char payload[2];
+  int packetLength = 0;
+
+  //dla statusu potencjometru i lampki, to samo - > odesłanie value w payload
+  sprintf(payload, "%u", value); //kopiowanie  value do payloadu
+  setResponseMessageFields(responseMessage, coapMessage, messageID, payload); //SPRAWIDZC CZY DOBRZE TE REFERENCJE SĄ PRZEKAZYWANE I OBIEKTY/TABLICE DO FUNKCJI :)
+  unsigned char * packet = responseMessage.toPacket(packetLength); // packetLength jest przekazywane przez referencję i jest zmieniane w funkcji na prawidlową wartosc
+  sendUdpResponse(coapMessage, packet, packetLength);
+  delete packet;
+}
+
+void handleRadioRequest(short option, short value)    //przeciążenie do obsługi zasobów o dużym czasie dostępu
+{
+
+}
+
+void sendUdpResponse(CoapMessage coapMessage, unsigned char * packet, int packetLength) {
+  Udp.beginPacket(coapMessage.getRemoteIPAddress(), coapMessage.getRemotePort());
+  Udp.write(packet, packetLength);
+  Udp.endPacket();
+}
+
+void setResponseMessageFields(CoapMessage responseMessage,  CoapMessage coapMessage, uint8_t messageID[], unsigned char payload[2]) {
+  responseMessage.setHeader(coapMessage.getToken(), coapMessage.getTokenLength(), CoapUtils::MessageType::NON, CoapUtils::ResponseCode::SUCCESS, CoapUtils::SuccessResponseCode::CONTENT, messageID);
+  responseMessage.setContentFormat(0);
+  responseMessage.setPayload(payload, sizeof(payload));
+}
+
+
---
+++ main-coap/main-coap-request-handling.ino
@@ -0,0 +1,119 @@
+
+//======COAP - GET REQUEST HANDLING==========
+
+
+void handleGetRequest(CoapMessage &coapMessage) {
+
+  String uriPath;
+  coapMessage.getUriPath(uriPath);
+  //  showDebug(coapMessage);
+  Serial.print("UriPath=");
+  Serial.println(uriPath);
+  // debugVar(uriPath);
+
+  if (uriPath == ".well-known/core") {
+    respondToWellKnownCoreGet(coapMessage);
+  }
+
+  if (uriPath == "Lampka") {
+    sendRequestViaRadio(LampStatus);                        //wyślij geta po status lampki
+    waitForResponseAndHandleIt(coapMessage);
+  }
+
+  if (uriPath == "Potencjometr") {
+    uint8_t observeOption = coapMessage.getObserve();
+    Serial.print("ObserveOption=");
+    Serial.println(observeOption);
+
+    if (observeOption) {
+      if (observeOption == 0) {                               // dodawanie do listy obserwatorow
+        addObserverForPotStatus(coapMessage);
+      }
+      if (observeOption == 1) {                           //  usuwanie z listy obserwatorow
+        removeObserverForPotStatus(coapMessage);
+      }
+    }
+    else {
+      sendRequestViaRadio(PotStatus);
+      waitForResponseAndHandleIt(coapMessage);
+    }
+  }
+}
+
+void handlePutRequest(CoapMessage &coapMessage) {
+  unsigned char* payload = coapMessage.getPayload();
+
+  showDebug(coapMessage); //in main-coap file :)
+  debugPayload(payload, coapMessage.getPayloadLength());
+
+  sendRequestViaRadio((short)(payload[0] - '0'));  // W MAIN-COAP-RADIO-COMMUNICATION.INO FILE :)
+
+}
+
+
+void respondToWellKnownCoreGet(CoapMessage &coapMessage) {
+  Serial.println("odkrywanie zasobow");
+  CoapMessage responseMessage;
+  uint16_t messageID = 3124;
+  String ledLamp = "</Lampka>;rt=\"led-lamp-status\";if=\"executable\";ct=0,";                   //zasoby
+  String potentiometer = "</potentiometer>;rt=\"led-lamp-status\";if=\"executable\";ct=0;obs,";
+  unsigned char payloadCharArray[ledLamp.length() + 1];
+  int packetLength = 0;
+  unsigned char * packet;
+
+  responseMessage.setHeader(coapMessage.getToken(), coapMessage.getTokenLength(), CoapUtils::MessageType::NON, CoapUtils::ResponseCode::SUCCESS, CoapUtils::SuccessResponseCode::CONTENT, messageID);
+  responseMessage.setContentFormat(40);
+
+  ledLamp.toCharArray((char*)payloadCharArray, ledLamp.length() + 1);
+
+  debugPayload(payloadCharArray, sizeof(payloadCharArray));  //from main-coap.ino
+  debugPayload(responseMessage.getPayload(), responseMessage.getPayloadLength());
+
+  responseMessage.setPayload(payloadCharArray, sizeof(payloadCharArray));
+
+  packet = responseMessage.toPacket(packetLength); // packetLength jest przekazywane przez referencję i jest zmieniane w funkcji na prawidlową wartosc
+  sendUdpResponse(coapMessage, packet, packetLength);
+  delete packet;
+
+}
+
+//czeka na odpowiedź od resourca i przekazuje ją do funkcji ją obsługującej
+void waitForResponseAndHandleIt(CoapMessage &coapMessage) {
+  boolean waitForResponse = true;
+
+  while (waitForResponse) {
+    Serial.println("przyszla odpowiedz z resourca");
+    network.update();
+    while (network.available()) {
+      Serial.println("Odebrano");
+      struct Response message;
+      RF24NetworkHeader header;
+      network.read(header, &message, sizeof(struct Response));
+      handleRadioRequest(message.option, message.value, coapMessage);
+    }
+    waitForResponse = false;
+  }
+}
+
+void addObserverForPotStatus(CoapMessage &coapMessage) {
+  Serial.println("W observe 0");
+  observersList[0].isEmpty = false;
+  observersList[0].remoteAddress = coapMessage.getRemoteIPAddress();
+  unsigned char * oToken;
+
+  memcpy(&oToken, coapMessage.getToken(), coapMessage.getTokenLength());
+  observersList[0].token = oToken;
+
+  sendRequestViaRadio(PotStatus);
+  Serial.println("Odpowiedz ze statusem potencjometru");
+  waitForResponseAndHandleIt(coapMessage);
+
+}
+
+void removeObserverForPotStatus(CoapMessage &coapMessage) {
+  //todo
+}
+
+
+
+
